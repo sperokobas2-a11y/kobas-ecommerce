75b15e13ee359419a940fb2a19449f4510ab32c4
sperokobas2-a11y <sperokobas2@gmail.com>
2026-09-03 03:57:46 +0100

    Refactor className for layout component

--- app/layout.tsx
+++ app/layout.tsx
@@ -88,7 +88,12 @@ export default function RootLayout({
   return (
     <html
       lang="fr"
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
+      className={
+        geistSans.variable +
+        " " +
+        geistMono.variable +
+        " h-full antialiased"
+      }
     >
       <body className="min-h-full flex flex-col">
         <Providers>{children}</Providers>
