c43302c0b7e19118a97310bc94351360072204ca
sperokobas2-a11y <sperokobas2@gmail.com>
2026-09-04 02:42:48 +0100

    Import Analytics component from Vercel

--- app/layout.tsx
+++ app/layout.tsx
@@ -98,6 +98,7 @@ export default function RootLayout({
     >
       <body className="min-h-full flex flex-col">
         <Providers>{children}</Providers>
+        import { Analytics } from "@vercel/analytics/next";
       </body>
     </html>
   );
