ec2a858286b30a94ae45293aa3deb9a4a8e1db21
sperokobas2-a11y <sperokobas2@gmail.com>
2026-09-04 12:57:56 +0100

    Update base URL in sitemap function

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -2,7 +2,7 @@ import type { MetadataRoute } from "next";
 import { prisma } from "@/lib/prisma";
 
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
-  const baseUrl = "https://kobas-ecommerce.vercel.app";
+  const baseUrl = "https://www.kobas-ecommerce.shop";
 
   const products = await prisma.product.findMany({
     where: {
